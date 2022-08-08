0f6f5d4e26d95808e6badab50c6d634542853d02
xFlest <mail@fle.st>
2022-08-09 02:03:03 +0900

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,20 +45,7 @@ export default {
         jQuery: 'jquery',
         $: 'jquery'
       })
-    ],
-    babel: {
-      presets({ isServer }) {
-        return [
-          [
-            require.resolve('@nuxt/babel-preset-app'),
-            {
-              buildTarget: isServer ? 'server' : 'client',
-              corejs: { version: 3 }
-            }
-          ]
-        ]
-      }
-    }
+    ]
   },
 
   srcDir: 'src/',
